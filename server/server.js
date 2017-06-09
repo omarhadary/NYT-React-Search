8bc202ca86e9b118efb41a1186eb218c04393662
Omar Hadary <Omar Hadary>
2017-06-09 02:45:19 -0700

    changes for heroku deployment

--- server/server.js
+++ server/server.js
@@ -26,7 +26,12 @@ app.use(bodyParser.urlencoded({
 }));
 
 // Make public a static dir
-app.use(express.static("public"));
+// app.use(express.static("public"));
+
+// get reference to the client build directory
+const staticFiles = express.static(path.join(__dirname, '../../client/build'))
+// pass the static files (react app) to the express app. 
+app.use(staticFiles)
 
 // Database configuration with mongoose
 mongoose.connect("mongodb://localhost/nytreact");
@@ -46,13 +51,21 @@ db.once("open", function() {
 // Routes
 
 app.get("/api/saved", (req, res) => {
-  const article = {
-    title: 'Ali Sells Jersey House And Moves to Chicago - why not update',
-    date: '1974-07-18T00:00:00Z',
-    url: 'http://query.nytimes.com/gst/abstract.html?res=9A0DE5D8173FEF34BC4052DFB166838F669EDE'
-  }
+  const article = [
+    {title: 'Ali Sells Jersey House And Moves to Chicago - why not update',
+    date: '1978-07-18T00:00:00Z',
+    url: 'http://query.nytimes.com/gst/abstract.html?res=9A0DE5D8173FEF34BC4052DFB166838F669EDE'},
+    {title: 'Omar Sells Jersey House And Moves to Chicago - why not update',
+    date: '1983-07-18T00:00:00Z',
+    url: 'http://query.nytimes.com/gst/abstract.html?res=9A0DE5D8173FEF34BC4052DFB166838F669EDE'},
+    {title: 'Noha Sells Jersey House And Moves to Chicago - why not update',
+    date: '1950-07-18T00:00:00Z',
+    url: 'http://query.nytimes.com/gst/abstract.html?res=9A0DE5D8173FEF34BC4052DFB166838F669EDE'}
+  ]
   res.json(article)
 })
+
+app.use('/*', staticFiles)
 
 // Set & listen on port 3001
 
